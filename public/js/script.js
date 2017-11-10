d7012c65baef8e32d2c08c1456fde96e8496f9b7
Ramkashyap <ram.kashyap117@gmail.com>
2017-11-09 20:03:55 -0700

    year-slider

--- public/js/script.js
+++ public/js/script.js
@@ -1,7 +1,9 @@
+let yearChart = new YearChart();
+yearChart.update();
+
 
 
 d3.csv("data/movie_metadata.csv", function (error, movies) {
-
     let movieTable = new MovieTable(movies);
     movieTable.create();
     movieTable.update();
